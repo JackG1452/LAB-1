e6e217e720a946066833f430a36a1620ea18f447
JackG1452 <43519919+JackG1452@users.noreply.github.com>
2019-01-29 12:07:35 +0000

    First

---
+++ Lab-1/Lab-1/stdafx.h
@@ -0,0 +1,15 @@
+//stdafx.h used to store standard and project specific header files
+// represents the start if the compile chain
+
+#pragma once
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN // Exclude rarely used stuff from windows headers
+#endif
+
+#include <Windows.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <d3dcompiler.h>
+#include "d3dx12.h"
+#include <stdio.h>
